89e7799d8f961e051781f047bd3cdbb7e2827b73
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-03-20 14:01:46 +0330

    create show model

--- app/Models/Show/Show.php
+++ app/Models/Show/Show.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Show extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'type',
+        'date_aired',
+        'status',
+        'genere',
+        'duration',
+        'quality'
+    ];
+    
 }
--- database/migrations/2024_03_19_082953_create_shows_table.php
+++ database/migrations/2024_03_19_082953_create_shows_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use function Laravel\Prompts\table;
 
 return new class extends Migration
 {
@@ -13,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('shows', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('genere');
+            $table->string('image');
+            $table->dateTime('date_aired');
+            $table->enum('status',['finished','airing']);
+            $table->string('duration');
+            $table->string('quality');
             $table->timestamps();
         });
     }
