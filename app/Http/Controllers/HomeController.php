9a92a6a9809eaf0595edabad10c45a6465b51ab6
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-03-20 14:03:04 +0330

    complete home controller and pass data to home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Show\Show;
+use \Illuminate\Support\Str;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,20 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $heros = Show::query()->inRandomOrder()->take(3)->get();
+        $heros->map(fn ($hero) => $hero->description = Str::words($hero->description, 15, '...'));
+
+        $trendingShows = Show::query()->inRandomOrder()->take(3)->get();
+
+        $actionShows = Show::query()->orderBy('id','desc')->where('genere', 'action')->take(4)->get();
+
+        $recentShows = Show::query()->orderBy('id','desc')->take(6)->get();
+
+        return view('home', [
+            'heros' => $heros,
+            'actionShows' => $actionShows,
+            'recentShows'=> $recentShows,
+            'trendingShows'=> $trendingShows
+        ]);
     }
 }
