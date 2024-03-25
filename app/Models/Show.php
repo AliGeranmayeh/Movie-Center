bf23899bb02d412141307a01f39270bf32b865f2
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-03-25 20:11:22 +0430

    create following list table

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Show extends Model
 {
@@ -27,4 +28,8 @@ class Show extends Model
         return $this->hasMany(Comment::class);
     }
     
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class , 'following_list');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -47,5 +48,10 @@ class User extends Authenticatable
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function shows(): BelongsToMany
+    {
+        return $this->belongsToMany(Show::class, 'following_list');
     }
 }
